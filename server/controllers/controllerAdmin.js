5a7497dd6061418da90e7963655665aefc986e3a
fuza-ail <mfuzail1991@gmail.com>
2020-06-02 03:09:01 +0700

    fancy algorithm done

--- server/controllers/controllerAdmin.js
+++ server/controllers/controllerAdmin.js
@@ -3,6 +3,9 @@ const jwt = require('jsonwebtoken')
 const { checkPassword } = require('../helpers/bcrypt');
 require('dotenv').config();
 const { Op } = require('sequelize');
+const Moment = require('moment');
+const {extendMoment} = require('moment-range');
+let moment = extendMoment(Moment)
 
 class ControllerAdmin {
 
@@ -55,7 +58,7 @@ class ControllerAdmin {
   static getUser(req, res, next) {
     const userId = req.params.id
     User.findByPk(userId, {
-      attributes: ['id','no_ktp', 'name', 'email', 'address', 'status', 'phone'],
+      attributes: ['id', 'no_ktp', 'name', 'email', 'address', 'status', 'phone'],
       include: {
         model: UserBarcode,
         attributes: ['checkin', 'checkout'],
@@ -76,7 +79,106 @@ class ControllerAdmin {
   }
 
   static updateUserStatus(req, res, next) {
+    const inputData = req.body
+    const userId = req.params.id
+    let suspectUser
+    const suspectHotplaces =[]
+    const sameHotplaceUser = []
+    const contactedUser = []
 
+    User.findByPk(userId)
+      .then(user => {
+        return User.update({
+          status: inputData.status
+        }, {
+          where: { id: user.id }
+        })
+      })
+      .then(() => {
+        return User.findByPk(userId, {
+          attributes: ['id', 'no_ktp', 'name'],
+          include: {
+            model: UserBarcode,
+            attributes: ['checkin', 'checkout'],
+            include: {
+              model: Barcode,
+              attributes: ['id','name', 'barcode_url']
+            }
+          }
+        })
+      })
+      .then(userData=>{
+        userData.UserBarcodes.forEach(el=>{
+          suspectHotplaces.push({
+            barcodeId : el.Barcode.id,
+            checkIn: el.checkin,
+            checkOut: el.checkout
+          })
+        })
+        let promises = []
+        suspectHotplaces.forEach(el=>{
+          promises.push(
+            Barcode.findOne({
+              where:{
+                id: el.barcodeId
+              },
+              include:{
+                model: UserBarcode
+              }
+            })
+          )
+        })
+        return Promise.all(promises)
+        
+      })
+      .then(data=>{
+        data.forEach(el=>{
+          let suspect={}
+          suspect.otherUser =[]
+          el.UserBarcodes.forEach(element=>{
+            if(element.UserId == Number(userId)){
+              suspect.checkIn = element.checkin
+              suspect.checkOut = element.checkout
+            }else{
+              suspect.otherUser.push({
+                userId: element.UserId,
+                checkIn: element.checkin,
+                checkOut: element.checkout
+              })
+            }
+          })
+          sameHotplaceUser.push(suspect)
+        })
+        sameHotplaceUser.forEach(el=>{
+          let suspectRange = moment.range(el.checkIn,el.checkOut)
+          el.otherUser.forEach(element=>{
+            let userRange = moment.range(element.checkIn,element.checkOut)
+            if(suspectRange.overlaps(userRange)){
+              contactedUser.push(element.userId)
+            }
+          })
+        })
+        let lastPromise = []
+        contactedUser.forEach(el=>{
+          lastPromise.push(
+            User.update({
+              status: 'OTG'
+            },{
+              where:{
+                id: el
+              }
+            })
+          )
+        })
+        Promise.all(lastPromise)
+        // res.status(200).json(contactedUser)
+      })
+      .then(()=>{
+        res.status(200).json({
+          message: 'all user has been updated'
+        })
+      })
+      .catch(error => next(error))
   }
 
   static getHotplaces(req, res, next) {
@@ -96,7 +198,7 @@ class ControllerAdmin {
   static getHotplace(req, res, next) {
     const hotplaceId = req.params.id
     Hotplace.findByPk(hotplaceId, {
-      attributes: ['id','name', 'type', 'address', 'phone'],
+      attributes: ['id', 'name', 'type', 'address', 'phone'],
       include: {
         model: Barcode,
         attributes: ['name', 'barcode_url'],
@@ -105,7 +207,7 @@ class ControllerAdmin {
           attributes: ['checkin', 'checkout'],
           include: {
             model: User,
-            attributes: ['name', 'email', 'address', 'phone','no_ktp']
+            attributes: ['id','name', 'email', 'address', 'phone', 'no_ktp']
           }
         }
       }
