bcd322549f9b46261f895cc2602205e685f994de
hasbif <hasbisenpai@gmail.com>
2020-06-03 01:07:49 +0700

    details page with user/hotplace table

--- client-admin/src/components/locationDetails.js
+++ client-admin/src/components/locationDetails.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
+import { Table } from 'react-bootstrap'
 import axios from 'axios'
 import url from '../url'
+import moment from 'moment'
 
 function LocationDetail() {
 
     const [data, setData] = useState('')
     const [loading, setLoading] = useState(true)
     let { locationId } = useParams()
+
+    moment.locale('en-sg')
 
     useEffect(() => {
         setLoading(true)
@@ -24,13 +28,43 @@ function LocationDetail() {
     }
 
 
-    return <div style={{ width: '85vw' }}>
-        {JSON.stringify(data)}
 
-        <h1>Name: {data.name}</h1>
-        <h2>Type: {data.type}</h2>
-        <h5>Address: {data.address}</h5>
-        <h5>Contact: {data.phone}</h5>
+
+    return <div style={{ width: '85vw', display: 'flex' }}>
+        <div style={{ flex: '1', }}>
+            <div style={{ marginLeft: '10%', marginTop: '2%' }}>
+                <h1>Name: {data.name}</h1>
+                <h2>Type: {data.type}</h2>
+                <h5>Address: {data.address}</h5>
+                <h5>Contact: {data.phone}</h5>
+                <img src={data.Barcode.barcode_url} alt="QR Code" width="200" height="200"></img>
+            </div>
+        </div>
+
+        <div style={{ flex: '3' }}>
+            <Table>
+                <thead style={{ color: '#00B979' }}>
+                    <tr>
+                        <th>Name</th>
+                        <th>KTP</th>
+                        <th>Check In</th>
+                        <th>Check Out</th>
+                    </tr>
+                </thead>
+                <tbody style={{ color: '#46B19C' }}>
+                    {data.Barcode.UserBarcodes.map((user, idx) => {
+                        return <tr key={idx}>
+                            <td>  <Link to={'/users/' + user.User.id}>{user.User.name}</Link> </td>
+                            <td> {user.User.no_ktp} </td>
+                            <td> {moment(user.checkin).format('MMMM Do YYYY, h:mm:ss a')} </td>
+                            <td> {moment(user.checkout).format('MMMM Do YYYY, h:mm:ss a')} </td>
+                        </tr>
+                    })}
+                </tbody>
+            </Table>
+            {JSON.stringify(data)}
+        </div>
+
 
     </div>
 
--- client-admin/src/components/userDetails.js
+++ client-admin/src/components/userDetails.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Form, Table } from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import axios from 'axios'
 import url from '../url'
+import moment from 'moment'
 
 function UserDetail() {
 
@@ -11,6 +12,8 @@ function UserDetail() {
     const [status, setStatus] = useState('')
     const [showForm, setShowForm] = useState(false)
     let { userId } = useParams()
+
+    moment.locale('en-sg')
 
     useEffect(() => {
         setLoading(true)
@@ -41,57 +44,54 @@ function UserDetail() {
     }
 
 
-    return <div style={{ width: '85vw' }}>
-        {JSON.stringify(data)}
+    return <div style={{ width: '85vw', display: 'flex' }}>
+        <div style={{ flex: '1', }}>
+            <div style={{ marginLeft: '10%', marginTop: '2%' }}>
+                <h1>Name: {data.name}</h1>
+                <h2>Status: {data.status}</h2>
+                <h5>KTP: {data.no_ktp}</h5>
+                <h5>Address: {data.address}</h5>
+                <h5>Contact: {data.phone}</h5>
 
-        <h1>Name: {data.name}</h1>
-        <h2>Status: {data.status}</h2>
-        <h5>KTP: {data.no_ktp}</h5>
-        <h5>Address: {data.address}</h5>
-        <h5>Contact: {data.phone}</h5>
+                {showForm ?
+                    <div>
+                        <Form.Control placeholder="Status" value={status} onChange={(e) => setStatus(e.target.value)} as="select">
+                            <option value="negatif">Negatif</option>
+                            <option value="positif">Positif</option>
+                            <option value="OTG"> OTG</option>
+                        </Form.Control>
+                        <Button onClick={updateStatus}>Confirm</Button>
+                        <Button onClick={() => { setShowForm(false); setStatus(data.status) }}>Cancel</Button> </div> :
+                    <Button onClick={() => setShowForm(true)}>Change Status</Button>
+                }
+            </div>
+        </div>
 
-
-
-        {showForm ?
-            <div>
-                <Form.Control placeholder="Status" value={status} onChange={(e) => setStatus(e.target.value)} as="select">
-                    <option value="negatif">Negatif</option>
-                    <option value="positif">Positif</option>
-                    <option value="OTG"> OTG</option>
-                </Form.Control>
-                <Button onClick={updateStatus}>Confirm</Button>
-                <Button onClick={() => { setShowForm(false); setStatus(data.status) }}>Cancel</Button> </div> :
-            <Button onClick={() => setShowForm(true)}>Change Status</Button>
-
-        }
-
-
-        <Table>
-            <thead style={{ color: '#00B979' }}>
-                <tr>
-                    <th>QR Code</th>
-                    <th>Location Name</th>
-                    <th>Location Type</th>
-                    <th>Check In</th>
-                    <th>Check Out</th>
-                </tr>
-            </thead>
-            <tbody style={{ color: '#46B19C' }}>
-                {data.UserBarcodes.map(barcode => {
-                    return <tr key={barcode.checkin}>
-                        <td> {barcode.Barcode.barcode_url} </td>
-                        <td> {barcode.Barcode.Hotplace.name} </td>
-                        <td> {barcode.Barcode.Hotplace.type} </td>
-                        <td> {barcode.checkin} </td>
-                        <td> {barcode.checkout} </td>
+        <div style={{ flex: '3' }}>
+            <Table>
+                <thead style={{ color: '#00B979' }}>
+                    <tr>
+                        <th>QR Code</th>
+                        <th>Location Name</th>
+                        <th>Location Type</th>
+                        <th>Check In</th>
+                        <th>Check Out</th>
                     </tr>
-                })}
-            </tbody>
-        </Table>
-
-
-
-
+                </thead>
+                <tbody style={{ color: '#46B19C' }}>
+                    {data.UserBarcodes.map(barcode => {
+                        return <tr key={barcode.checkin}>
+                            <td>  <img src={barcode.Barcode.barcode_url} alt="QR Code" width="100" height="100"></img></td>
+                            <td>  <Link to={'/locations/' + barcode.Barcode.Hotplace.id}>{barcode.Barcode.Hotplace.name}</Link> </td>
+                            <td> {barcode.Barcode.Hotplace.type} </td>
+                            <td> {moment(barcode.checkin).format('MMMM Do YYYY, h:mm:ss a')} </td>
+                            <td> {moment(barcode.checkout).format('MMMM Do YYYY, h:mm:ss a')} </td>
+                        </tr>
+                    })}
+                </tbody>
+            </Table>
+            {JSON.stringify(data)}
+        </div>
 
 
     </div>
