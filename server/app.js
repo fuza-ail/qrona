7c4ab9a4a3ff0365fb4be34dae8973053a2e5e51
abdmhamzah <iamhamzaham@gmail.com>
2020-06-03 21:17:25 +0700

    update config for deploy to heroku second time

--- server/app.js
+++ server/app.js
@@ -5,11 +5,11 @@ const app = express();
 const router = require("./routes");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use(router);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
-})
+});
